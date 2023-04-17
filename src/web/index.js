2551d8f8dd3dc5f83b4f3d97974b71adff5be943
tutu <1137725646@qq.com>
2023-04-17 21:14:20 +0800

    change ws origin

--- src/web/index.js
+++ src/web/index.js
@@ -1,5 +1,4 @@
 let hostname = window.location.hostname;
 export default {
-    // ws: new WebSocket(`ws://${hostname}:8000/ws`),
-    ws: new WebSocket("ws://127.0.0.1:8000/ws"),
+    ws: new WebSocket(`ws://${hostname}/ws`),
 }
